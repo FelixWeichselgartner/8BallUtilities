6e2b95128406a2cdb8316c9368cf0ab585ccb854
Felix Weichselgartner <felix.weichselgartner@t-online.de>
2023-03-21 22:48:01 +0100

    optimize bill splitter

--- BillSplitter.py
+++ BillSplitter.py
@@ -1,5 +1,5 @@
-import time
 import datetime
+import time
 from paypal_me import paypal_me_url
 
 
@@ -10,29 +10,24 @@ players = {
     'Toni': '18:46-21:41',
 }
 
-
 # 10 percent tip
 bill = 36.8 * 1.1
 
 player_list = players.keys()
-player_time = dict()
-overall_time = datetime.date.today() - datetime.date.today()
+player_time = {}
 
+overall_time = datetime.timedelta()
 
 for p in player_list:
     s, e = players[p].split('-')
-    st = time.strptime(s, '%H:%M')
-    en = time.strptime(e, '%H:%M')
-    start = datetime.time(st.tm_hour, st.tm_min)
-    end = datetime.time(en.tm_hour, en.tm_min)
-    dateTimeStart = datetime.datetime.combine(datetime.date.today(), start)
-    dateTimeEnd = datetime.datetime.combine(datetime.date.today(), end)
-    delta = dateTimeEnd - dateTimeStart
+    start = datetime.datetime.strptime(s, '%H:%M').time()
+    end = datetime.datetime.strptime(e, '%H:%M').time()
+    delta = datetime.datetime.combine(datetime.date.today(), end) - datetime.datetime.combine(datetime.date.today(), start)
     overall_time += delta
     player_time[p] = delta
-    
 
 bill_sum = 0
+
 for p in player_list:
     b = round(player_time[p] / overall_time * bill, 2)
     bill_sum += b
